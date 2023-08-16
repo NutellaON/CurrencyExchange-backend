4710c0813d7ce76ca1d5057bb902441e1833ca11
alanscertkojevs@scandiweb.com <alans.certkojevs@gmail.com>
2023-08-16 14:11:34 +0300

    models + migrations

---
+++ database/migrations/2023_08_16_102045_create_exchange_rates_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exchange_rates', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('currency_id');
+            $table->decimal('rate', 10, 4);
+            $table->date('date');
+            $table->timestamps();
+            $table->foreign('currency_id')->references('id')->on('currencies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exchange_rates');
+    }
+};
